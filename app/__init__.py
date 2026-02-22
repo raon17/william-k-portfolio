b7177c8a7446f096ca033c529755343ccdefd39f
William Kim <willkim0117@gmail.com>
2026-02-22 19:59:21 +1100

    Html Css update

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template
 
 def create_app():
-    app = Flask(__name__)
-
+    app = Flask(__name__) 
     @app.route('/')
     def index():
         return render_template('index.html')
