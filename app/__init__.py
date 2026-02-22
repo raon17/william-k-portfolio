cdb265324eea900e1b5e22b5cfb65cd615ff7f5a
William Kim <willkim0117@gmail.com>
2026-02-22 15:36:54 +1100

    General push

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 def create_app():
     app = Flask(__name__)
 
     @app.route("/")
     def home():
-        return "Hello, my personal website!"
-
+        return render_template("index.html")
     return app
