f2367d6f8db97cea5d57047cf99c7bf6a23fc102
William Kim <willkim0117@gmail.com>
2026-02-22 15:47:25 +1100

    General update

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,8 @@ from flask import Flask, render_template
 def create_app():
     app = Flask(__name__)
 
-    @app.route("/")
-    def home():
-        return render_template("index.html")
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
     return app
